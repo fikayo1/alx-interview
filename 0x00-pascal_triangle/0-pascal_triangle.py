0255e4cf8a742db1f886cc88f2fa3a584e97439a
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-04-24 01:19:17 +0100

    Added pacal triangle function

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    pas = []
+
+    for i in range(n):
+        r_List = [1]
+        if i == 0:
+            pas.append(r_List)
+            continue
+
+        k = i - 1
+        for j in range(len(pas[k])):
+            if j + 1 == len(pas[k]):
+                r_List.append(1)
+                break
+            # adding the result of previous row and col
+            res = pas[k][j] + pas[k][j + 1]
+            r_List.append(res)
+        pas.append(r_List)
+
+    return pas
