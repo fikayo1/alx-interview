6aa7751e2d14904d1d3e02a1b65172d9619f4a6c
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-06-18 00:21:45 +0100

    Interview solution added

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,6 @@ Change Comes from within
 def makeChange(coins, total):
     """Returns the fewest number of coins needed to meet total."""
     if total < 1:
-        
         return 0
     tot = 0
     for i in range(len(coins)):
@@ -19,5 +18,3 @@ def makeChange(coins, total):
             return -1
         coins.remove(coin)
     return tot
-    
-print(makeChange([1256, 54, 48, 16, 102], 1453))
